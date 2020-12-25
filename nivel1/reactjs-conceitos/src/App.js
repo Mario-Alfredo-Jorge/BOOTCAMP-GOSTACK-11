0410ac7040fdccea1e0686bbc14cbce0f42b5d1f
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2020-12-25 18:41:41 +0300

    State and imutability

--- nivel1/reactjs-conceitos/src/App.js
+++ nivel1/reactjs-conceitos/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 import Header from './components/Header';
 
 const App = () => {
+
+  const [propjects, setProjects] = useState(['Development of App', 'front-end mobile']);
+
+  const handleNewPropject = () => {
+    setProjects([...propjects, `New propject ${new Date()}`]);
+  }
   return (
     <>
-      <Header  title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Project</li>
-          <li>Login</li>
-        </ul>
-      </Header>
-      <Header title="Project">
-        <ul>
-          <li>Homepage</li>
-          <li>Project</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Project" />
+      <ul>
+        {
+          propjects.map(propject => (
+            <li key={propject}>
+              {propject}
+            </li>
+          ))
+        }
+      </ul>
+
+      <button type="button" onClick={handleNewPropject}>Add new Project</button>
     </>
   );
 };
