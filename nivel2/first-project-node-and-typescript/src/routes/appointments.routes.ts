98e7dc7b0dce273a8ade7920beb4b6a7f068512a
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2020-12-28 14:43:44 +0300

    learn repository concept

--- nivel2/first-project-node-and-typescript/src/routes/appointments.routes.ts
+++ nivel2/first-project-node-and-typescript/src/routes/appointments.routes.ts
@@ -2,28 +2,32 @@ import { Router } from 'express';
 import { startOfHour, parseISO, isEqual } from 'date-fns'
 
 import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRepository = new AppointmentsRepository();
 
 
 const AppointmentRoute = Router();
 
-const appointments: Appointment[] = [];
+AppointmentRoute.get('/', ( req, res ) => {
+
+  const appointments = appointmentsRepository.returnAll();
+  return res.json(appointments);
+})
+
 AppointmentRoute.post('/', ( req, res ) => {
 
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date)
-  );
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
   if(findAppointmentInSameDate){
     return res.status(400).json({ error: 'This appointment is already booked!' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
 });
