967d01c8e63afdc02fc89ff73e94b148800caf58
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2020-12-28 14:47:33 +0300

    learn repository concept

--- nivel2/first-project-node-and-typescript/src/models/Appointment.ts
+++ nivel2/first-project-node-and-typescript/src/models/Appointment.ts
@@ -8,7 +8,7 @@ class Appointment {
   date: Date;
 
 
-  constructor(provider: string, date:Date){
+  constructor({ provider,  date}: Omit<Appointment, 'id'>){
     this.id = uniqid();
     this.provider = provider;
     this.date = date;
