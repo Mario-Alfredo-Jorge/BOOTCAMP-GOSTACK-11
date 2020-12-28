4350febe612e2a86f06a7f999b58d771ada137e7
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2020-12-28 13:25:34 +0300

    learn how to work with lib date-fns

--- nivel2/first-project-node-and-typescript/src/routes/appointments.routes.ts
+++ nivel2/first-project-node-and-typescript/src/routes/appointments.routes.ts
@@ -1,21 +1,38 @@
 import { Router } from 'express';
 import uniqid from 'uniqid';
+import { startOfHour, parseISO, isEqual } from 'date-fns'
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date
+}
 
 
 const AppointmentRoute = Router();
 
-const Appointment = [];
+const appointments: Appointment[] = [];
 AppointmentRoute.post('/', ( req, res ) => {
 
   const { provider, date } = req.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if(findAppointmentInSameDate){
+    return res.status(400).json({ error: 'This appointment is already booked!' });
+  }
+
   const appointment = {
     id: uniqid(),
     provider,
-    date,
+    date: parsedDate,
   }
 
-  Appointment.push(appointment);
+  appointments.push(appointment);
 
   return res.json(appointment);
 });
--- nivel2/first-project-node-and-typescript/src/server.ts
+++ nivel2/first-project-node-and-typescript/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import routes from './routes'
 const server = express();
+server.use(express.json())
 
 server.use(routes);
 
