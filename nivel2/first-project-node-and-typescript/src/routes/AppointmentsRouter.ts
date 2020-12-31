aaf366f6e2988cb1bac88de379c5e01c3609e2fc
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2020-12-31 22:09:22 +0300

    Software architecture concepts

--- nivel2/first-project-node-and-typescript/src/routes/AppointmentsRouter.ts
+++ nivel2/first-project-node-and-typescript/src/routes/AppointmentsRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentsServices from '../services/CreateAppointmentsServices';
 
 const appointmentsRepository = new AppointmentsRepository();
 
@@ -12,26 +13,24 @@ AppointmentsRouter.get('/', (req, res) => {
 });
 
 AppointmentsRouter.post('/', (req, res) => {
-  const { provider, date } = req.body;
+  try {
+    const { provider, date } = req.body;
 
-  const parseDate = startOfHour(parseISO(date));
+    const appointmentDate = parseISO(date);
 
-  const findAppointmentInTheSameDate = appointmentsRepository.findByDate(
-    parseDate,
-  );
+    const createAppointment = new CreateAppointmentsServices(
+      appointmentsRepository,
+    );
 
-  if (findAppointmentInTheSameDate) {
-    return res.status(400).json({
-      error: 'This appointment is already booked, try in another date',
+    const appointment = createAppointment.execute({
+      date: appointmentDate,
+      provider,
     });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parseDate,
-  });
-
-  return res.json(appointment);
 });
 
 export default AppointmentsRouter;
