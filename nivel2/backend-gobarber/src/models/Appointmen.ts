b30e1343e4d2af0f76883bd1c796b0d394c2229f
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-01-01 19:26:05 +0300

    Create appointments model

--- nivel2/backend-gobarber/src/models/Appointmen.ts
+++ nivel2/backend-gobarber/src/models/Appointmen.ts
@@ -1,17 +1,15 @@
-import uniqid from 'uniqid';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uniqid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
